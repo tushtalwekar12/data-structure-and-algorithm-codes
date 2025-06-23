f0e2cf1bfbb1b89be1fa226c23baa87c5d2fbc0f
Tushar Talwekar <tushartalwekar1234@gmail.com>
2025-06-23 11:44:17 +0530

    adding files

--- src/section_01_function_and_methods/Example1.java
+++ src/section_01_function_and_methods/Example1.java
@@ -1,12 +1,28 @@
 package section_01_function_and_methods;
 // syntax - function - block of code , reusable code
 public class Example1 {
-    public static void printHelloWorld(){
+    // write printHelloWorld Function -
+    public static void printHelloWorld()
+    {
         System.out.println("Hello World");
     }
 
-    public static void main(String [] args){
+    // Write calculateSum() function
+    public static int calculateSum(int num1 ,int num2) // passing parameters or formal parameter
+    {
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public static void main(String [] args)
+    {
         printHelloWorld();  // call to function
+
+        int a =2;
+        int b =3;
+        int Sum = calculateSum(a,b);  // passing Argument or actual parameter
+        System.out.println("The sum is : "+ Sum);
+
     }
 
 }
--- src/section_03_arrays/Example1.java
+++ src/section_03_arrays/Example1.java
@@ -2,20 +2,32 @@ package section_03_arrays;
 // date : 05-02-2025
 // Topic : Array as function argument - array are pass by reference
 public class Example1 {
-    public static void update(int marks[]){
-        // loop
-        for(int i=0; i<marks.length; i++){
-            marks[i] = marks[i] + 1;
+    // write function for add 1 in an given array
+    public static void addOneInAnArray(int[] marks)
+    {
+        for (int i=0; i< marks.length; i++)
+        {
+            marks[i] =marks[i] + 1;
         }
+
     }
     public static void main(String[] args) {
-        int marks [] = {97,98,99};
-        update(marks);
-
-        // print marks array
-        for (int i=0; i< marks.length; i++){
-            System.out.println(marks[i] + " ");
+        int[] marks = {97,98,99}; // create an array
+        System.out.println("Before Update :");
+        for (int i =0; i< marks.length; i++)
+        {
+            System.out.print(marks[i]+ " ");
         }
         System.out.println();
+        // call function
+        addOneInAnArray(marks);
+
+        // print an array
+        System.out.println("After Update :");
+        for (int i =0; i< marks.length; i++)
+        {
+            System.out.print(marks[i]+ " ");
+        }
+
     }
 }
